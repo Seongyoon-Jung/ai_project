1d84be0bbaa4f979ad06537dd3e813d76c5b4c91
Seongyoon-Jung <blurblauer@gmail.com>
2024-07-04 00:16:47 +0900

    update socket-test

--- socket_test/client.py
+++ socket_test/client.py
@@ -1,8 +1,8 @@
 import socket
 
-# 컴퓨터(서버)의 IP 주소와 포트 번호
-HOST = '컴퓨터의_IP주소'  # 예: '192.168.0.10'
-PORT = 65432              # 서버에서 사용한 동일한 포트 번호
+# 서버(컴퓨터)의 IP 주소와 포트 번호
+HOST = '192.168.0.42'  # 맥북의 IP 주소
+PORT = 65432           # 서버에서 사용한 동일한 포트 번호
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
