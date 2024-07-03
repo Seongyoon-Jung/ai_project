538fde37ddc316b519c12e6003d61598b899471f
Seongyoon-Jung <blurblauer@gmail.com>
2024-07-03 23:39:22 +0900

    bluetooth added

--- server.py
+++ server.py
@@ -1,39 +1,16 @@
-# bluetooth_server.py
-import bluetooth
+import asyncio
+from bleak import BleakServer
 
-def start_server():
-    server_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    port = bluetooth.PORT_ANY
-    server_socket.bind(("", port))
-    server_socket.listen(1)
+def notification_handler(sender, data):
+    print(f"Received message: {data}")
+    if data == b'person_passed':
+        print('passed')
 
-    port = server_socket.getsockname()[1]
-    bluetooth.advertise_service(server_socket, "SampleServer",
-                                service_id=str(port),
-                                service_classes=[bluetooth.SERIAL_PORT_CLASS],
-                                profiles=[bluetooth.SERIAL_PORT_PROFILE])
+async def main():
+    server = BleakServer(notification_handler)
+    await server.start()
+    print(f"Server started and listening for connections")
+    await server.wait_for_disconnect()
+    print("Server stopped")
 
-    print(f"Waiting for connection on RFCOMM channel {port}")
-
-    client_socket, client_info = server_socket.accept()
-    print(f"Accepted connection from {client_info}")
-
-    try:
-        while True:
-            data = client_socket.recv(1024)
-            if not data:
-                break
-            message = data.decode()
-            print(f"Received message: {message}")
-            if message == 'person_passed':
-                print('passed')
-    except OSError:
-        pass
-
-    print("Disconnected.")
-
-    client_socket.close()
-    server_socket.close()
-
-if __name__ == "__main__":
-    start_server()
+asyncio.run(main())
