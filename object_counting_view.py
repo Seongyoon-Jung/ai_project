3aa09edf6837ca6695f5c441ab27e802c2e70bab
Seongyoon-Jung <blurblauer@gmail.com>
2024-07-05 14:26:50 +0900

    socket revised

--- object_counting_view.py
+++ object_counting_view.py
@@ -50,7 +50,7 @@ def main(weights="yolov8n.pt", source=0, save_output=False, server_ip="192.168.0
 
         # Use the Object Counter to count objects in the frame and get the annotated image
         im0, counts = counter.start_counting(im0, tracks)
-
+        print(counts)
         # Check if the count has increased
         if counts["total"] > previous_count:
             client_socket.sendall(b"passed")
--- socket_test/client.py
+++ socket_test/client.py
@@ -1,7 +1,7 @@
 import socket
 
 # 서버(컴퓨터)의 IP 주소와 포트 번호
-HOST = '192.168.0.42'  # 맥북의 IP 주소
+HOST = '141.223.108.158' # 서버의 IP 주소
 PORT = 65432           # 서버에서 사용한 동일한 포트 번호
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
